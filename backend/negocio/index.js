6094c28c0a7b4becc4b138c56656b87ece5779d5
Sofii19 <nicole.gomez31993@ucaldas.edu.co>
2025-08-01 23:51:33 -0500

    Union de cliente con reservas en un microservicio único

--- backend/negocio/index.js
+++ backend/negocio/index.js
@@ -9,16 +9,19 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+
 // Importar rutas
 const reservaRoutes = require('./src/routes/reserva.route');
 const pagoRoutes = require('./src/routes/pago.route');
+const clienteRoutes = require('./src/routes/cliente.routes');
 
 // Usar rutas
 app.use('/api/reservas', reservaRoutes);
 app.use('/api/pagos', pagoRoutes);
+app.use('/api/clientes', clienteRoutes);
 
 // Iniciar servidor
 
 app.listen(PORT, () => {
-  console.log(`🟢 Microservicio de reservas y pagos corriendo en http://localhost:${PORT}`);
+  console.log(`Microservicio de lógica de negocio corriendo en http://localhost:${PORT}`);
 });
--- backend/negocio/sql/init.sql
+++ backend/negocio/sql/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE reservas_db;
 
 CREATE TABLE reservas (
   id SERIAL PRIMARY KEY,
-  cliente_id INTEGER NOT NULL,
+  cliente_id INTEGER NOT NULL REFERENCES clientes(id) ON DELETE CASCADE,
   paquete_id INTEGER NOT NULL,
   fecha_reserva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   cantidad_personas INTEGER NOT NULL CHECK (cantidad_personas > 0),
@@ -15,4 +15,13 @@ CREATE TABLE pagos (
   metodo_pago VARCHAR(50) NOT NULL,
   monto NUMERIC(10,2) NOT NULL,
   fecha_pago TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE clientes (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  correo VARCHAR(100) NOT NULL UNIQUE,
+  telefono VARCHAR(20),
+  fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  activo BOOLEAN DEFAULT TRUE
 );
